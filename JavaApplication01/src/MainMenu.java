9b165f34e1aa3b93d982a140cde9a61277028bf6
LasithEranga <73494729+LasithEranga@users.noreply.github.com>
2020-11-17 18:47:08 +0530

    Added new println

--- JavaApplication01/src/MainMenu.java
+++ JavaApplication01/src/MainMenu.java
@@ -12,6 +12,7 @@ public class MainMenu {
     public static void main(String[] args) {
         for (int i=0; i<100; i++){
             System.out.println("The current value of i is:"+i);
+            System.out.println("The current value of i is:"+i);
         }
         
     }

06b135b9343c5cfc149e260adcee508a9da61d5c
LasithEranga <lasitheranga1@gmail.com>
2020-11-17 18:20:48 +0530

    Application 5 added

--- JavaApplication01/src/MainMenu.java
+++ JavaApplication01/src/MainMenu.java
@@ -11,5 +11,8 @@
 public class MainMenu {
     public static void main(String[] args) {
         System.out.println("Hello");
+        for (int i=0; i<100; i++){
+            System.out.println("The current value of i is:"+i);
+        }
     }
 }
